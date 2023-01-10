952fe731fdb3cc0ce71d4e8b76653ae2a99f0829
YoungSeoKim98 <dudtj9809@gmail.com>
2023-01-10 18:10:09 +0900

    230110

--- Final_Project/src/main/java/com/mycompany/webapp/travel/dao/TravelDao.java
+++ Final_Project/src/main/java/com/mycompany/webapp/travel/dao/TravelDao.java
@@ -6,11 +6,34 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.mycompany.webapp.place.model.PagerVo;
 import com.mycompany.webapp.place.model.PlaceVo;
+import com.mycompany.webapp.travel.model.TravelVo;
 
 @Mapper
 public interface TravelDao {
 	
-	public int count();
-	public List<PlaceVo> selectPlaceList();  //전체관광지리스트페이징처리
+	public int countTravel();
+	
+	public List<PlaceVo> selectPlaceList();
+
+	public List<PlaceVo> selectPlaceListByArea(String gugun1);
+	
+	//public List<TravelVo> selectTravelList(); //글 목록
+	
+	public List<TravelVo> selectTravelList(PagerVo pager); //페이징처리 글 목록
+
+	public List<TravelVo> selectTravelListByView(PagerVo pager); //조회수 순 리스트
+
+	public List<TravelVo> selectTravelListByShare(PagerVo pager); //공유수 순 리스트
+
+	public List<TravelVo> selectTravelListByRecent(PagerVo pager); //최신순 리스트
+
+	
+
+
+	
+	
+	
+	
+
 
 }
