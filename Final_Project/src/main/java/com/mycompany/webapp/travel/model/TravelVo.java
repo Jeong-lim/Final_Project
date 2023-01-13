a0eb5ebea7add776d5b85e9ec8e4df036e5b903e
YoungSeoKim98 <dudtj9809@gmail.com>
2023-01-13 18:15:15 +0900

    230113 travellist 수정

--- Final_Project/src/main/java/com/mycompany/webapp/travel/model/TravelVo.java
+++ Final_Project/src/main/java/com/mycompany/webapp/travel/model/TravelVo.java
@@ -18,117 +18,106 @@ public class TravelVo {
 	private String travelDate;
 	private String placeId;
 	private String memo;
-
+	private String placeName;
+	private String fileSavedName;
 	public String getTravelId() {
 		return travelId;
 	}
-
 	public void setTravelId(String travelId) {
 		this.travelId = travelId;
 	}
-
 	public String getTravelTitle() {
 		return travelTitle;
 	}
-
 	public void setTravelTitle(String travelTitle) {
 		this.travelTitle = travelTitle;
 	}
-
 	public String getTravelStart() {
 		return travelStart;
 	}
-
 	public void setTravelStart(String travelStart) {
 		this.travelStart = travelStart;
 	}
-
 	public String getTravelEnd() {
 		return travelEnd;
 	}
-
 	public void setTravelEnd(String travelEnd) {
 		this.travelEnd = travelEnd;
 	}
-
 	public char getTravelPrivacy() {
 		return travelPrivacy;
 	}
-
 	public void setTravelPrivacy(char travelPrivacy) {
 		this.travelPrivacy = travelPrivacy;
 	}
-
 	public char getFree() {
 		return free;
 	}
-
 	public void setFree(char free) {
 		this.free = free;
 	}
-
 	public int getViewCnt() {
 		return viewCnt;
 	}
-
 	public void setViewCnt(int viewCnt) {
 		this.viewCnt = viewCnt;
 	}
-
 	public int getShareCnt() {
 		return shareCnt;
 	}
-
 	public void setShareCnt(int shareCnt) {
 		this.shareCnt = shareCnt;
 	}
-
 	public String getWriter() {
 		return writer;
 	}
-
 	public void setWriter(String writer) {
 		this.writer = writer;
 	}
-
 	public Date getWriteDate() {
 		return writeDate;
 	}
-
 	public void setWriteDate(Date writeDate) {
 		this.writeDate = writeDate;
 	}
-
 	public String getTravelDate() {
 		return travelDate;
 	}
-
 	public void setTravelDate(String travelDate) {
 		this.travelDate = travelDate;
 	}
-
 	public String getPlaceId() {
 		return placeId;
 	}
-
 	public void setPlaceId(String placeId) {
 		this.placeId = placeId;
 	}
-
 	public String getMemo() {
 		return memo;
 	}
-
 	public void setMemo(String memo) {
 		this.memo = memo;
 	}
-
+	public String getPlaceName() {
+		return placeName;
+	}
+	public void setPlaceName(String placeName) {
+		this.placeName = placeName;
+	}
+	public String getFileSavedName() {
+		return fileSavedName;
+	}
+	public void setFileSavedName(String fileSavedName) {
+		this.fileSavedName = fileSavedName;
+	}
 	@Override
 	public String toString() {
 		return "TravelVo [travelId=" + travelId + ", travelTitle=" + travelTitle + ", travelStart=" + travelStart
 				+ ", travelEnd=" + travelEnd + ", travelPrivacy=" + travelPrivacy + ", free=" + free + ", viewCnt="
 				+ viewCnt + ", shareCnt=" + shareCnt + ", writer=" + writer + ", writeDate=" + writeDate
-				+ ", travelDate=" + travelDate + ", placeId=" + placeId + ", memo=" + memo + "]";
+				+ ", travelDate=" + travelDate + ", placeId=" + placeId + ", memo=" + memo + ", placeName=" + placeName
+				+ ", fileSavedName=" + fileSavedName + "]";
 	}
 
+	
 }
