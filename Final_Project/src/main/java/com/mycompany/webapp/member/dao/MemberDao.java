ed3dcf21a00182624bf7e97de8cce9c0863a4d82
jgy <wjdrkdud01@gmail.com>
2023-02-09 14:32:12 +0900

    0209---가영 코드정리

--- Final_Project/src/main/java/com/mycompany/webapp/member/dao/MemberDao.java
+++ Final_Project/src/main/java/com/mycompany/webapp/member/dao/MemberDao.java
@@ -42,8 +42,7 @@ public interface MemberDao {
 	public void insertCheckList(@Param("memberId") String memberId, @Param("Item") String Item); // 체크리스트 등록
 	public List<Map<String, String>> selectCheckList(String memberId);
 	public void resetCheckList(@Param("memberId") String memberId);
-	public void updateCheckList(@Param("memberId") String memberId, @Param("status") String status,
-			@Param("item") String item);
+	public void updateCheckList(@Param("memberId") String memberId, @Param("status") String status,@Param("item") String item);
 	public void deleteCheckList(@Param("memberId") String memberId, @Param("checkId") String checkId);
 	
 }
